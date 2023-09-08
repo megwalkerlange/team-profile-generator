d19773a3861431b5d8150acef523d40564e2bf4c
Meg Walker <meg.walker56@gmail.com>
2023-09-08 19:40:25 +0100

    added npm, array fo questions and started making creator objects/methods

--- starter/index.js
+++ starter/index.js
@@ -10,6 +10,82 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questionsManager = [
+  {
+    name: "name",
+    message: "Please type the name of manager",
+  },
+
+  {
+    name: "ID",
+
+    message: "Please type manager's ID",
+  },
+  {
+    name: "e-mail",
+    message: "Please type manager's e-mail address",
+  },
+  {
+    name: "office",
+    message: "Please type the room number of manager's office",
+  },
+  {
+    type: "list",
+    message: "Please select a job role for next employee",
+    choices: ["Engineer", "Intern", "Finish building team"],
+  },
+];
+
+const questionsEngineer = [
+  {
+    name: "name",
+    message: "Please type the name of engineer",
+  },
+  { name: "e-mail", message: "Please type engineer's e-mail address" },
+  {
+    name: "ID",
+
+    message: "Please type engineer's ID",
+  },
+  {
+    name: "github",
+    message: "Please type engineers gitHib username",
+  },
+  {
+    type: "list",
+    message: "Please select a job role for employee",
+    choices: ["Engineer", "Intern", "Finish building team"],
+  },
+];
+
+const questionsIntern = [
+  {
+    name: "name",
+    message: "Please type the name of intern",
+  },
+  { name: "e-mail", message: "Please type intern's e-mail address" },
+  {
+    name: "ID",
+
+    message: "Please type intern's ID",
+  },
+  {
+    name: "university",
+    message: "Please type the name of intern's university",
+  },
+  {
+    type: "list",
+    message: "Please select a job role for employee",
+    choices: ["Engineer", "Intern", "Finish building team"],
+  },
+];
+
+function init() {
+  inquirer
+    .prompt(questionsManager, questionsEngineer, questionsIntern)
+    .then((answers) => console.log("Creating README.md File..."));
+}
+
+init();
--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,40 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email, role) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = role;
+  }
+}
+
+getName(){
+  if (this.name === "") {
+    return this.name;
+  } else {
+    console.log("Please enter a valid name!");
+  }
+};
+
+getId(){
+  if (this.id === "") {
+    return console.log("Please enter a valid number");
+  }
+
+  if (this.id > 0 || this.id < 100) {
+    return;
+  }
+};
+// getEmail()
+// {
+
+// }
+
+// getRole(){
+
+// }
+
+Employee.getName();
+Employee.getId();
+
+module.exports = Employee;
