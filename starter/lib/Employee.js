b2bba524d0b7eebd4af6ee5732fcdc659097224a
Meg Walker <meg.walker56@gmail.com>
2023-09-10 22:46:04 +0100

    trying to get tests to work

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,27 +1,30 @@
 // TODO: Write code to define and export the Employee class
+const inquirer = require("inquirer");
+
 class Employee {
   constructor(name, email) {
     this.name = name;
-    this.id = 0;
+    this.id = id;
     this.email = email;
   }
 
   getName() {
-    if (this.name === "") {
-      return this.name;
-    } else {
-      console.log("Please enter a valid name!");
-    }
+    return inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "Please type the name of Employee",
+        },
+      ])
+      .then((answers) => {
+        this.name = answers.name;
+      });
   }
 
   getId() {
-    if (this.id === "") {
-      return console.log("Please enter a valid number");
-    }
-
-    if (this.id > 0 || this.id < 100) {
-      return;
-    }
+    const idValue = 100;
+    const newEmployee = new Employee("Bill", idValue);
+    return;
   }
 
   getEmail() {
@@ -36,8 +39,8 @@ class Employee {
   }
 }
 
-const newEmployee = new Employee(require("../index"({ ...answers })));
+// const newEmployee = new Employee(require("../index"({ ...answers })));
 
-console.log(newEmployee);
+// console.log(newEmployee);
 
 module.exports = Employee;
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,8 +1,23 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
+const inquirer = require("inquirer");
+
 class Engineer extends Employee {
   constructor(github) {
-    return this.github;
+    this.github = githib;
+  }
+
+  getGithub() {
+    return inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "Please type the name of Engineers Github username",
+        },
+      ])
+      .then((answers) => {
+        this.githib = answers.githib;
+      });
   }
 }
